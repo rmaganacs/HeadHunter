e866031725ee79ad5154faff31b42f1e79d223a5
Ricardo Magana <rmaganacs@gmail.com>
2022-07-29 23:07:26 -0400

    Added Tabs in the Center of the page
    Added tabs for future pages in the center of the tab. Also needed an image buffer on the right side of the Toolbar in order to get it to perfectly center for the Tabs.

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,27 +2,54 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { Icon } from '@mui/material';
 import IconLogo from '../images/icon.png';
-import {Menu, MenuItem} from '@mui/material';
-
-const pages = ['Charcters', 'Maps', 'Guns'];
+import BufferLogo from '../images/buffer-icon.png';
+import {Menu, MenuItem, Button, Grid, Tab, Tabs} from '@mui/material';
+import {Icon} from '@mui/material'
 const ResponsiveAppBar = () => {
-
+  const [value, setValue] = React.useState('one');
+  const styles = theme => ({
+    indicator: {
+      backgroundColor: 'white',
+    },
+  })
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <Box>
       <AppBar color="transparent" elevation={0}>
-        <Container maxWidth="xl">
-          <Toolbar>
-          <Box
-            component="img"
-            src={IconLogo}
-          />
-          <Box sx={{ mx: "auto", letterSpacing: 4 }} >
-            <Typography variant='h6' color="common.white">CHARACTERS</Typography>
-          </Box>
+        <Container maxWidth="xxl">
+          <Toolbar sx={{justifyContent:'space-between'}}>
+            <Box
+              sx={{ ml: "-2"}}
+              component="img"
+              src={IconLogo}
+            />
+            <Box sx={{ mx: "auto"}}>
+            <Tabs
+              sx={{"& button:hover": { color: "#DCDBDB" }}}
+              textColor='white'
+              TabIndicatorProps={{
+                style: {
+                  backgroundColor: "#eb263a"
+                 }
+                }}
+              value={value} 
+              onChange={handleChange} 
+              aria-label="Main Tabs"
+            >
+              <Tab disableRipple value="one" sx={{color: 'white', fontSize: '21px'}} label="CHARACTERS"/>
+              <Tab disableRipple sx={{color: 'white', fontSize: '21px'}} label="MAPS"/>
+              <Tab disableRipple sx={{color: 'white', fontSize: '21px'}} label="WEAPONS"/>
+            </Tabs>
+            </Box>
+            <Box
+              sx={{ mr: "-2"}}
+              component="img"
+              src={BufferLogo}
+            />
           </Toolbar>
         </Container>
       </AppBar>
